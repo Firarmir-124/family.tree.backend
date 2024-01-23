afe8c3071d813f503215cc2f6b411636f1a9d912
Arsen <orozbaev.arsen@gmail.com>
2024-01-23 15:20:07 +0600

    material init

--- src/modules/materials/materials.module.ts
+++ src/modules/materials/materials.module.ts
@@ -2,15 +2,14 @@ import {Module} from "@nestjs/common";
 import {MaterialGroupEntity} from "./entities/material-group.entity";
 import {MaterialEntity} from "./entities/material.entity";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {MaterialService} from "./services/material.service";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MaterialEntity, MaterialGroupEntity]),
   ],
   controllers: [],
-  providers: [
-
-  ],
-  exports: [],
+  providers: [MaterialService],
+  exports: [MaterialService],
 })
 export class MaterialsModule {}
