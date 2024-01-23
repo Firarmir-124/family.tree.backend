86b9d1814331b5156a21f8eac37658235b3b5b3d
Темирлан Буларов <temirlanbularov@MacBook-Air-Temirlan.local>
2024-01-23 15:48:23 +0600

    fix

--- src/modules/information/controllers/information.public.controller.ts
+++ src/modules/information/controllers/information.public.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { InformationEntity } from '../entities/information.entity';
 import { ApiTags } from '@nestjs/swagger';
 import { IdParamDto } from '../../../global/dtos/id-param.dto';
+import { ApiFindOneInformation } from '../decorators/find-one-information.decorator';
 
 @Controller({
   version: '1',
@@ -12,6 +13,7 @@ export class InformationPublicController {
   constructor() {}
 
   @Get(':id')
+  @ApiFindOneInformation()
   public async findOneOrFail(
     @Param() params: IdParamDto,
   ): Promise<InformationEntity> {
