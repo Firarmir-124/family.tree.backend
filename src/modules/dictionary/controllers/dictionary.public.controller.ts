67690b82d51a07cb59ff7b9bfb827e3b2c33c2bb
Polushin Dima <dima2004polushin2004@gmail.com>
2024-02-08 11:42:10 +0600

    #task_1 Добавил @ApiProperty в схему dictionary

--- src/modules/dictionary/controllers/dictionary.public.controller.ts
+++ src/modules/dictionary/controllers/dictionary.public.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DictionaryService } from '../services/dictionary.service';
 import { DictionaryType } from '../interfaces/dictionary-type.interface';
+import { DictionaryEntity } from '../entities/dictionary.entity';
 
 @Controller({
   version: '1',
@@ -11,6 +12,11 @@ import { DictionaryType } from '../interfaces/dictionary-type.interface';
 export class DictionaryPublicController {
   constructor(private readonly dictionaryService: DictionaryService) {}
   @Get('distinct')
+  @ApiResponse({
+    description: 'положительный ответ',
+    status: HttpStatus.OK,
+    type: [DictionaryEntity],
+  })
   async distinct() {
     return await this.dictionaryService.findAll(DictionaryType.DISTRICT);
   }
--- src/modules/dictionary/entities/dictionary.entity.ts
+++ src/modules/dictionary/entities/dictionary.entity.ts
@@ -3,20 +3,25 @@
 // Compare this snippet from src/modules/estate/entities/estate.entity.ts:
 
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'dictionary',
 })
 export class DictionaryEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   type: string;
 
+  @ApiProperty()
   @Column()
   key: string;
 
+  @ApiProperty()
   @Column()
   value: string;
 }
